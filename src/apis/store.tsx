155f6dd45de7bd51307c4369a12d098d726d61ad
Ezequiel Korenblum <ezequiel@losko.com.ar>
2021-06-08 00:03:41 -0300

    Store is now persistent

--- src/apis/store.tsx
+++ src/apis/store.tsx
@@ -1,11 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
 import sessionReducer from "./session/session.slice";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+	key: "root",
+    storage
+};
+
+const perSessionReducer = persistReducer(persistConfig, sessionReducer);
 
 export const store = configureStore({
 	reducer: {
-		session: sessionReducer,
+		session: perSessionReducer,
 	},
 });
+
+const persistor = persistStore(store)
 
 export type RootState = ReturnType<typeof store.getState>;
 
